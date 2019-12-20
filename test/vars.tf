315ea39c3d439979da8ffb7de0db52a22ea44fc7
aakashchughsrijan <58848904+aakashchughsrijan@users.noreply.github.com>
2019-12-20 14:17:11 +0530

    Update vars.tf

--- test/vars.tf
+++ test/vars.tf
@@ -1,8 +1,6 @@
 variable "region"{
  default = "us-east-2"
 }
-
-<<<<<<< HEAD
 variable "access_key"{}
 
 variable "secret_key"{}
@@ -13,18 +11,9 @@ default = {
  us-east-2 = "ami-0c64dd618a49aeee8"
  us-west-1 = "ami-0c64dd618a49a"
 }
-=======
-variable "access_key"{
- default = "access-key-value"
->>>>>>> ee8157b7a29ad27cc3a423eeb0fe94ee184f8fe2
-}
+
 variable "vpc_cidr" {}
 
-<<<<<<< HEAD
 variable "vpc_tenancy" {
  default = "default"
-=======
-variable "secret_key"{
- default = "secret-key-value"
->>>>>>> ee8157b7a29ad27cc3a423eeb0fe94ee184f8fe2
 }
