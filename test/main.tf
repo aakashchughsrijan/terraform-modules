c16c3f01087bdd0d406960c61faa6b222dc3d00b
aakashchughsrijan <aakash.chugh@srijan.net>
2019-12-20 13:24:36 +0530

    using vars in modules

--- test/main.tf
+++ test/main.tf
@@ -1,13 +1,7 @@
-provider "aws"{
-  region = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
-
 module "my_vpc"{
 source = "../modules/vpc"
-vpc_cidr = "192.168.0.0/16"
-tenancy = "default"
+vpc_cidr = var.vpc_cidr
+tenancy = var.vpc_tenancy
 enable_dns_support = true
 enable_dns_hostnames = false
 enable_classiclink = false
@@ -19,7 +13,7 @@ name-tag = "MY-Test-VPC"
 module "my_ec2"{
 source = "../modules/ec2"
 ec2_count = "1"
-ami_id = "ami-0c64dd618a49aeee8"
+ami_id = lookup(var.ami_id, var.region)
 instance_type = "t2.micro"
 }
 
--- test/vars.tf
+++ test/vars.tf
@@ -2,10 +2,19 @@ variable "region"{
  default = "us-east-2"
 }
 
-variable "access_key"{
- default = "AKIAYACZBDGGVUZR2TB2"
-}
+variable "access_key"{}
 
-variable "secret_key"{
- default = "s5Ev1C+JneGsLRLHhcftLqt0inWvwNvkkG4MnJ+8"
+variable "secret_key"{}
+
+variable "ami_id"{
+type = map
+default = {
+ us-east-2 = "ami-0c64dd618a49aeee8"
+ us-west-1 = "ami-0c64dd618a49a"
+}
+}
+variable "vpc_cidr" {}
+
+variable "vpc_tenancy" {
+ default = "default"
 }
